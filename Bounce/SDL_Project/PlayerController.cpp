b97d3cf0192eb71e51c6f4f6a56e02449b42ee88
Ricky Nguyen <aznricky12@hotmail.com>
2020-01-29 13:27:49 -0500

    Player Movement Basic
    Added some very basic character movement.

---
+++ Bounce/SDL_Project/PlayerController.cpp
@@ -0,0 +1,82 @@
+#include "PlayerController.h"
+
+PlayerController::PlayerController()
+{
+}
+
+
+PlayerController::~PlayerController()
+{
+}
+
+void PlayerController::Update()
+{
+	if (event.type == SDL_KEYDOWN) {
+		
+		switch(event.key.keysym.sym) {
+			case SDLK_d: //going right
+				body.ApplyForce(Vec3(100.0f, 0.0f, 0.0f));
+				break;
+			case SDLK_a: //going left
+				body.ApplyForce(Vec3(-100.0f, 0.0f, 0.0f));
+				break;
+			case  SDLK_w: //jumping
+				body.ApplyForce(Vec3(0.0f, 100.0f, 0.0f));
+				//jumping method
+				break;
+			case SDLK_RIGHT:
+				/*for the second character
+				body2.ApplyForce(Vec3(100.0f, 0.0f, 0.0f));
+				break;
+				*/
+			case SDLK_LEFT:
+				/*for the second character
+				body2.ApplyForce(Vec3(-100.0f, 0.0f, 0.0f));
+				break;
+				*/
+			case SDLK_UP:
+				/*for the second character
+				body2.ApplyForce(Vec3(0.0f, 100.0f, 0.0f));
+				break;
+				*/
+			case SDLK_f: //intracting with objects
+				break;
+			default:
+				break;
+		}
+		//This is for when the button released
+		if (event.type == SDL_KEYUP) {
+			switch (event.key.keysym.sym) {
+			case SDLK_d: 
+				body.ApplyForce(Vec3(0.0f, 0.0f, 0.0f));
+				break;
+			case SDLK_a: //going left
+				body.ApplyForce(Vec3(0.0f, 0.0f, 0.0f));
+				break;
+			case  SDLK_w: //jumping
+				body.ApplyForce(Vec3(0.0f, 0.0f, 0.0f));
+				//jumping method
+				break;
+			case SDLK_RIGHT:
+				/*for the second character
+				body2.ApplyForce(Vec3(0.0f, 0.0f, 0.0f));
+				break;
+				*/
+			case SDLK_LEFT:
+				/*for the second character
+				body2.ApplyForce(Vec3(0.0f, 0.0f, 0.0f));
+				break;
+				*/
+			case SDLK_UP:
+				/*for the second character
+				body2.ApplyForce(Vec3(0.0f, 0.0f, 0.0f));
+				break;
+				*/
+			case SDLK_f: //intracting with objects
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
---
+++ Bounce/SDL_Project/PlayerController.h
@@ -0,0 +1,19 @@
+#ifndef PLAYERCONTROLLER_H
+#define PLAYERCONTROLLER_H
+
+#include "Body.h"
+#include "SDL.h"
+
+class PlayerController
+{
+public:
+	SDL_Event event;
+	Body body;
+
+	PlayerController();
+	~PlayerController();
+
+	void Update();
+};
+
+#endif // PLAYERCONTOLLER_H
